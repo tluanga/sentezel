b55124d964a5e4c7756bc06f862f46c63427badc
tluanga <tluanga@live.com>
2021-08-10 06:34:10 +0530

    a

---
+++ lib/settings/transactionType/data/sumChetvelDanType_enum.dart
@@ -0,0 +1,15 @@
+enum TransactionTypeEnum {
+  lei,
+  hralh,
+  lakluh,
+  pekchhuah,
+  debtRepaymentByDebtor,
+  debtRepaymentByCreditor,
+  bankToCash,
+  cashToBank,
+  purchaseReturn,
+  saleReturn,
+  capitalInjection,
+  openingBalanceCash,
+  openingBalanceBank,
+}
