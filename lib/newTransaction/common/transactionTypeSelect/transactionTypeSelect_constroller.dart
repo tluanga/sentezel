8c7cbb443329e1ec7c765dc65a8fdfc56b9f5f30
tluanga <tluanga@live.com>
2021-08-31 05:40:37 +0530

    a

--- lib/newTransaction/common/transactionTypeSelect/transactionTypeSelect_constroller.dart
+++ lib/newTransaction/common/transactionTypeSelect/transactionTypeSelect_constroller.dart
@@ -1,6 +1,23 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sentezel/common/enums/sumChetvelDanType_enum.dart';
 import 'package:sentezel/settings/transactionType/data/transactionType_model.dart';
+import 'package:sentezel/settings/transactionType/transactionType_repository.dart';
 
-class TransactionTypeSelectController extends StateNotifier<TransactionType> {
-  TransactionTypeSelectController() : super();
+class TransactionTypeSelectController
+    extends StateNotifier<AsyncValue<List<TransactionType>>> {
+  final Reader _read;
+  TransactionTypeSelectController(this._read) : super(AsyncValue.loading());
+  init() async {
+    final data = await _read(transactionTypeRepositoryProvider)
+        .getTransactionTypeListBySumChetvelDanType(SumChetvelDanType.lakluh);
+    state = AsyncValue.data(data);
+  }
+
+  search(String searchParam) async {
+    final data = await _read(transactionTypeRepositoryProvider)
+        .getTransactionTypeListBySumChetvelDanType(SumChetvelDanType.lakluh);
+    List<TransactionType> filteredData =
+        data.where((element) => element.name.contains(searchParam)).toList();
+    state = AsyncValue.data(filteredData);
+  }
 }
