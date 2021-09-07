09d90f0308d9bdaae1654ab5df50a1d57554c0d3
tluanga <tluanga@live.com>
2021-09-07 06:27:46 +0530

    a

--- lib/newTransaction/purchase/purchaseOfAsset/purchaseOfAsset_screen.dart
+++ lib/newTransaction/purchase/purchaseOfAsset/purchaseOfAsset_screen.dart
@@ -7,6 +7,7 @@ import 'package:sentezel/common/ui/widget/topBarWithSave_widget.dart';
 import 'package:sentezel/newTransaction/common/partialPayment_widget.dart';
 import 'package:sentezel/newTransaction/common/assetSelect_modal.dart';
 import 'package:sentezel/newTransaction/data/transactionMode_enum.dart';
+import 'package:sentezel/newTransaction/purchase/purchaseOfAsset/model/purchaseOfAsset_model.dart';
 import 'package:sentezel/newTransaction/purchase/purchaseOfAsset/purchaseOfAssetConfirm_modal.dart';
 import 'package:sentezel/newTransaction/purchase/purchaseOfAsset/purchaseOfAssetTransactionModeSelect_modal.dart';
 import 'package:sentezel/newTransaction/purchase/purchaseOfAsset/purchaseOfAsset_controller.dart';
@@ -56,69 +57,9 @@ class PurchaseOfAssetScreen extends HookConsumerWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     //--------------Amount ------------
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.38,
-                      height: MediaQuery.of(context).size.height * 0.1,
-                      child: TextFormField(
-                        //
-                        onChanged: (value) {
-                          ref
-                              .watch(purchaseOfAssetControllerProvider.notifier)
-                              .setState(
-                                state.copyWith(
-                                  amount: int.parse(value),
-                                ),
-                              );
-                        },
-                        decoration: InputDecoration(
-                          labelText: 'Amount',
-                        ),
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        keyboardType: TextInputType.number,
-                      ),
-                    ),
+                    _amount(context: context, state: state, ref: ref),
                     SizedBox(width: MediaQuery.of(context).size.width * 0.02),
-                    //------------Transaction Mode----
-                    GestureDetector(
-                      onTap: () {
-                        showModalBottomSheet(
-                          context: context,
-                          builder: (context) =>
-                              PurchaseOfAssetTransactionModeSelectModalBottomSheet(),
-                        );
-                      },
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.55,
-                        height: MediaQuery.of(context).size.height * 0.05,
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade300,
-                          borderRadius: BorderRadius.circular(3),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              EnumToString.convertToString(state.mode,
-                                  camelCase: true),
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Icon(
-                              CupertinoIcons.arrowtriangle_down,
-                              color: Colors.black,
-                              size: 20,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+                    _transactionMode(context: context, state: state),
                   ],
                 ),
 
@@ -133,72 +74,13 @@ class PurchaseOfAssetScreen extends HookConsumerWidget {
                                   TransactionMode.partialPaymentByBank ||
                               state.mode ==
                                   TransactionMode.partialPaymentByCash)
-                            PartialPaymentWidget(
-                              onChange: (amount) {
-                                ref
-                                    .watch(purchaseOfAssetControllerProvider
-                                        .notifier)
-                                    .setState(
-                                      state.copyWith(
-                                          partialPaymentAmount: amount),
-                                    );
-                              },
-                              defaultValue: state.partialPaymentAmount,
-                              maxAmount: state.amount,
-                            ),
-                          GestureDetector(
-                            onTap: () {
-                              showModalBottomSheet(
-                                context: context,
-                                builder: (context) => PartySelectModal(
-                                  onSelectParty: (party) {
-                                    ref
-                                        .watch(purchaseOfAssetControllerProvider
-                                            .notifier)
-                                        .setState(
-                                          state.copyWith(partyLedger: party),
-                                        );
-                                  },
-                                ),
-                              );
-                            },
-                            child: Container(
-                              width: state.mode == TransactionMode.credit
-                                  ? MediaQuery.of(context).size.width * 0.97
-                                  : MediaQuery.of(context).size.width * 0.55,
-                              height: MediaQuery.of(context).size.height * 0.05,
-                              padding: EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                  color: Colors.grey.shade300,
-                                  borderRadius: BorderRadius.circular(3),
-                                  border: Border.all(
-                                    color: state.partyLedger != 0
-                                        ? Colors.grey.shade300
-                                        : Colors.red.shade300,
-                                  )),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    state.partyLedger == null
-                                        ? 'Please Select Party'
-                                        : state.partyLedger!.name,
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Icon(
-                                    CupertinoIcons.arrowtriangle_down,
-                                    color: Colors.black,
-                                    size: 20,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
+                            //Partial Payment
+                            _partialPayment(
+                                context: context, state: state, ref: ref),
+
+                          //---Party Select----
+                          _partySelect(
+                              context: context, state: state, ref: ref),
                         ],
                       )
                     : Container(),
@@ -206,68 +88,9 @@ class PurchaseOfAssetScreen extends HookConsumerWidget {
                   height: 10,
                 ),
                 //-----ASSET SELECTION----------------------
-                GestureDetector(
-                  onTap: () {
-                    showModalBottomSheet(
-                      context: context,
-                      builder: (context) => AssetSelectModal(
-                        onSelect: (asset) {
-                          ref
-                              .watch(purchaseOfAssetControllerProvider.notifier)
-                              .setState(
-                                state.copyWith(assetLedger: asset),
-                              );
-                        },
-                      ),
-                    );
-                  },
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.97,
-                    height: MediaQuery.of(context).size.height * 0.05,
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade300,
-                      borderRadius: BorderRadius.circular(3),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          state.assetLedger == null
-                              ? 'Please Select Asset'
-                              : state.assetLedger!.name,
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Icon(
-                          CupertinoIcons.arrowtriangle_down,
-                          color: Colors.black,
-                          size: 20,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.95,
-                  height: MediaQuery.of(context).size.height * 0.1,
-                  child: TextFormField(
-                    initialValue: state.particular,
-                    onChanged: (value) {
-                      ref
-                          .watch(purchaseOfAssetControllerProvider.notifier)
-                          .setState(
-                            state.copyWith(particular: value),
-                          );
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'particular',
-                    ),
-                  ),
-                ),
+                _assetSelect(context: context, state: state, ref: ref),
+                //----PARTICULAR SELECTION----------
+                _particular(context: context, state: state, ref: ref),
                 SizedBox(
                   height: 6,
                 ),
@@ -288,6 +111,215 @@ class PurchaseOfAssetScreen extends HookConsumerWidget {
           ref.watch(purchaseOfAssetControllerProvider.notifier).submit();
         },
         onCancel: () {},
+      ),
+    );
+  }
+
+  _amount(
+      {required BuildContext context,
+      required PurchaseOfAsset state,
+      required WidgetRef ref}) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.38,
+      height: MediaQuery.of(context).size.height * 0.1,
+      child: TextFormField(
+        //
+        onChanged: (value) {
+          ref.watch(purchaseOfAssetControllerProvider.notifier).setState(
+                state.copyWith(
+                  amount: int.parse(value),
+                ),
+              );
+        },
+        decoration: InputDecoration(
+          labelText: 'Amount',
+        ),
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+        keyboardType: TextInputType.number,
+      ),
+    );
+  }
+
+  _transactionMode(
+      {required BuildContext context, required PurchaseOfAsset state}) {
+    return //------------Transaction Mode----
+        GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) =>
+              PurchaseOfAssetTransactionModeSelectModalBottomSheet(),
+        );
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.55,
+        height: MediaQuery.of(context).size.height * 0.05,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.grey.shade300,
+          borderRadius: BorderRadius.circular(3),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              EnumToString.convertToString(state.mode, camelCase: true),
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Icon(
+              CupertinoIcons.arrowtriangle_down,
+              color: Colors.black,
+              size: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _assetSelect(
+      {required BuildContext context,
+      required PurchaseOfAsset state,
+      required WidgetRef ref}) {
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) => AssetSelectModal(
+            onSelect: (asset) {
+              ref.watch(purchaseOfAssetControllerProvider.notifier).setState(
+                    state.copyWith(assetLedger: asset),
+                  );
+            },
+          ),
+        );
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.97,
+        height: MediaQuery.of(context).size.height * 0.05,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.grey.shade300,
+          borderRadius: BorderRadius.circular(3),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              state.assetLedger == null
+                  ? 'Please Select Asset'
+                  : state.assetLedger!.name,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Icon(
+              CupertinoIcons.arrowtriangle_down,
+              color: Colors.black,
+              size: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _partySelect(
+      {required BuildContext context,
+      required PurchaseOfAsset state,
+      required WidgetRef ref}) {
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) => PartySelectModal(
+            onSelectParty: (party) {
+              ref.watch(purchaseOfAssetControllerProvider.notifier).setState(
+                    state.copyWith(partyLedger: party),
+                  );
+            },
+          ),
+        );
+      },
+      child: Container(
+        width: state.mode == TransactionMode.credit
+            ? MediaQuery.of(context).size.width * 0.97
+            : MediaQuery.of(context).size.width * 0.55,
+        height: MediaQuery.of(context).size.height * 0.05,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Colors.grey.shade300,
+            borderRadius: BorderRadius.circular(3),
+            border: Border.all(
+              color: state.partyLedger != 0
+                  ? Colors.grey.shade300
+                  : Colors.red.shade300,
+            )),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              state.partyLedger == null
+                  ? 'Please Select Party'
+                  : state.partyLedger!.name,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Icon(
+              CupertinoIcons.arrowtriangle_down,
+              color: Colors.black,
+              size: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _partialPayment(
+      {required BuildContext context,
+      required PurchaseOfAsset state,
+      required WidgetRef ref}) {
+    return PartialPaymentWidget(
+      onChange: (amount) {
+        ref.watch(purchaseOfAssetControllerProvider.notifier).setState(
+              state.copyWith(partialPaymentAmount: amount),
+            );
+      },
+      defaultValue: state.partialPaymentAmount,
+      maxAmount: state.amount,
+    );
+  }
+
+  _particular(
+      {required BuildContext context,
+      required PurchaseOfAsset state,
+      required WidgetRef ref}) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.95,
+      height: MediaQuery.of(context).size.height * 0.1,
+      child: TextFormField(
+        initialValue: state.particular,
+        onChanged: (value) {
+          ref.watch(purchaseOfAssetControllerProvider.notifier).setState(
+                state.copyWith(particular: value),
+              );
+        },
+        decoration: InputDecoration(
+          labelText: 'particular',
+        ),
       ),
     );
   }
