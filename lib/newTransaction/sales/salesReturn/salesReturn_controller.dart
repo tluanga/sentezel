7bcbc7c6eef97abb6f17af8b294c79af310addce
tluanga <tluanga@live.com>
2021-09-07 21:26:01 +0530

    sales return stared

---
+++ lib/newTransaction/sales/salesReturn/salesReturn_controller.dart
@@ -0,0 +1,123 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sentezel/newTransaction/common/helper/getTransactionModeLedger_helper.dart';
+import 'package:sentezel/newTransaction/data/transactionMode_enum.dart';
+import 'package:sentezel/newTransaction/data/transaction_model.dart';
+import 'package:sentezel/newTransaction/data/transaction_repository.dart';
+import 'package:sentezel/newTransaction/sales/salesReturn/model/salesReturn_model.dart';
+import 'package:sentezel/settings/ledgerMaster/ledgerMaster_repository.dart';
+import 'package:sentezel/settings/transactionCategory/data/transactionCategory_index.dart';
+import 'package:sentezel/settings/transactionCategory/data/transactionCategory_model.dart';
+import 'package:sentezel/settings/transactionCategory/transactionCategory_repository.dart';
+
+final saleReturnControllerProvider =
+    StateNotifierProvider<SalesReturnController, AsyncValue<SalesReturn>>(
+        (ref) => SalesReturnController(ref.read)..loadData());
+
+class SalesReturnController extends StateNotifier<AsyncValue<SalesReturn>> {
+  final Reader _read;
+
+  SalesReturnController(this._read) : super(AsyncValue.loading());
+
+  loadData() async {
+    TransactionCategory _category =
+        await _read(transactionCategoryRepositoryProvider)
+            .getItem(id: TransactionCategoryIndex.SalesReturn);
+    final _creditSideLedger = await _read(ledgerMasterRepositoryProvider)
+        .getItem(id: _category.creditSideLedger);
+
+    state = AsyncData(SalesReturn(
+      category: _category,
+      errorMessages: [],
+      date: DateTime.now(),
+      creditSideLedger: _creditSideLedger,
+      particular: _category.name,
+    ));
+    print(state);
+  }
+
+  //--------------SET STATE-------------
+  setState(payload) {
+    SalesReturn _newState = payload;
+    state = AsyncData(_newState);
+  }
+
+  validate() {
+    final stateData = state.data!.value;
+
+    //---------Validation Part-----------
+    List<String> _errorMessage = [];
+
+    if (stateData.amount <= 0) {
+      _errorMessage.add('Amount can not be less than equalto Zero');
+    }
+
+    if (stateData.mode == TransactionMode.partialPaymentByBank ||
+        stateData.mode == TransactionMode.partialPaymentByCash) {
+      if (stateData.partyLedger == null) {
+        _errorMessage.add('Please Select Party');
+      }
+      if (stateData.amount <= stateData.partialPaymentAmount) {
+        _errorMessage
+            .add('Partial Payment Amount cannot be Larger  than Amount');
+      }
+      if (stateData.partialPaymentAmount <= 0) {
+        _errorMessage.add('Partial Amount cannot be Zero');
+      }
+      if (stateData.particular!.length <= 0) {
+        _errorMessage.add('Please Enter Particular');
+      }
+    }
+    if (stateData.mode == TransactionMode.credit &&
+        stateData.partyLedger == null) {
+      _errorMessage.add('Please Select Party');
+    }
+
+    print('length of error message ${_errorMessage}');
+    state = AsyncData(stateData.copyWith(errorMessages: _errorMessage));
+  }
+
+  setup() async {
+    //---------------Updating the state-------
+    final stateData = state.data!.value;
+    final finalData = stateData.copyWith(
+      creditAmount: (stateData.amount - stateData.partialPaymentAmount),
+      debitAmount: stateData.amount,
+      debitSideLedger: stateData.mode != TransactionMode.credit
+          ? await getTransactionModeLedgerHelper(
+              stateData.mode!,
+              _read,
+            )
+          : null,
+    );
+    state = AsyncData(finalData);
+  }
+
+  reset() async {
+    state = AsyncLoading();
+  }
+
+  submit() async {
+    final stateData = state.data!.value;
+    try {
+      _read(transactionRepositoryProvider).add(
+        payload: Transaction(
+          debitAmount: stateData.debitAmount,
+          creditAmount: stateData.creditAmount,
+          partialPaymentAmount: stateData.partialPaymentAmount,
+          particular: stateData.particular!,
+          mode: stateData.mode!,
+          date: stateData.date,
+          transactionCategoryId: TransactionCategoryIndex.PurchaseOfRawMaterial,
+          debitSideLedger: stateData.debitSideLedger!.id,
+          creditSideLedger: stateData.creditSideLedger != null
+              ? stateData.creditSideLedger!.id
+              : null,
+          partyLedgerId:
+              stateData.partyLedger != null ? stateData.partyLedger!.id : null,
+        ),
+      );
+    } catch (e) {
+      print(e);
+    }
+  }
+}
---
+++ lib/newTransaction/sales/salesReturn/salesReturn_screen.dart
@@ -0,0 +1,305 @@
+import 'package:enum_to_string/enum_to_string.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:sentezel/common/ui/widget/dateSelectTimeLine_widget.dart';
+import 'package:sentezel/common/ui/widget/topBarWithSave_widget.dart';
+import 'package:sentezel/newTransaction/common/partialPayment_widget.dart';
+import 'package:sentezel/newTransaction/data/transactionMode_enum.dart';
+import 'package:sentezel/newTransaction/newTransactionCenter_screen.dart';
+
+import 'package:sentezel/newTransaction/sales/generalSales/model/generalSales_model.dart';
+import 'package:sentezel/newTransaction/sales/salesReturn/model/salesReturn_model.dart';
+import 'package:sentezel/newTransaction/sales/salesReturn/salesReturnConfirm_modal.dart';
+import 'package:sentezel/newTransaction/sales/salesReturn/salesReturnTransactionModeSelect_modal.dart';
+import 'package:sentezel/newTransaction/sales/salesReturn/salesReturn_controller.dart';
+import 'package:sentezel/newTransaction/sales/salesReturn/salesReturnvalidationError_bottomSheet.dart';
+import 'package:sentezel/settings/party/partySelect_modal.dart';
+
+class SalesReturnScreen extends HookConsumerWidget {
+  const SalesReturnScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var state = ref.watch(saleReturnControllerProvider);
+
+    onCancel() {
+      print('cancel is called');
+      ref.read(saleReturnControllerProvider.notifier).reset();
+    }
+
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 5,
+            ),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: state.when(
+                data: (data) {
+                  print(data);
+                  return Column(
+                    children: [
+                      TopBarWithSaveWidget(
+                        title: 'New Material Purchase',
+                        onSave: () {
+                          onSubmit(context: context, ref: ref);
+                        },
+                        onCancel: onCancel,
+                      ),
+                      DateSelectTimeLineWidget(
+                        initialDate: data.date,
+                        onDateSelected: (selectedDate) {
+                          ref
+                              .read(saleReturnControllerProvider.notifier)
+                              .setState(
+                                data.copyWith(date: selectedDate),
+                              );
+                        },
+                      ),
+
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.01,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          //--------------Amount ------------
+                          _amount(context: context, ref: ref),
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.02),
+                          _transactionMode(context: context, state: data),
+                        ],
+                      ),
+
+                      //------------Party Selection and Partial Amount Entry----
+                      data.mode == TransactionMode.credit ||
+                              data.mode ==
+                                  TransactionMode.partialPaymentByBank ||
+                              data.mode == TransactionMode.partialPaymentByCash
+                          ? Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                if (data.mode ==
+                                        TransactionMode.partialPaymentByBank ||
+                                    data.mode ==
+                                        TransactionMode.partialPaymentByCash)
+                                  //Partial Payment
+                                  _partialPayment(context: context, ref: ref),
+
+                                //---Party Select----
+                                _partySelect(context: context, ref: ref),
+                              ],
+                            )
+                          : Container(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      //-----ASSET SELECTION----------------------
+
+                      //----PARTICULAR SELECTION----------
+                      _particular(context: context, ref: ref),
+                      SizedBox(
+                        height: 6,
+                      ),
+                    ],
+                  );
+                },
+                loading: () {
+                  return CircularProgressIndicator();
+                },
+                error: (error, stack) {}),
+          ),
+        ),
+      ),
+    );
+  }
+
+  onSubmit({required BuildContext context, required WidgetRef ref}) async {
+    ref.watch(saleReturnControllerProvider.notifier).validate();
+    await ref.watch(saleReturnControllerProvider.notifier).setup();
+    final state = ref.watch(saleReturnControllerProvider);
+    if (state.data!.value.errorMessages.length == 0) {
+      showModalBottomSheet(
+        context: context,
+        builder: (context) => SalesReturnConfirmationBottomSheet(
+          onConfirm: () {
+            ref.watch(saleReturnControllerProvider.notifier).submit();
+
+            showCupertinoModalBottomSheet(
+              expand: true,
+              context: context,
+              backgroundColor: Colors.transparent,
+              builder: (context) => NewTranscationCenterScreen(),
+            );
+          },
+          onCancel: () {},
+        ),
+      );
+    } else {
+      showModalBottomSheet(
+        context: context,
+        builder: (context) => SalesReturnValidationErrorBottomSheet(
+          validationErrorMessages: state.data!.value.errorMessages,
+        ),
+      );
+    }
+  }
+
+  _amount({required BuildContext context, required WidgetRef ref}) {
+    final state = ref.watch(saleReturnControllerProvider);
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.38,
+      height: MediaQuery.of(context).size.height * 0.1,
+      child: TextFormField(
+        //
+        onChanged: (value) {
+          var _value = value != '' ? int.parse(value) : 0;
+          print(value);
+          ref.watch(saleReturnControllerProvider.notifier).setState(
+                state.data!.value.copyWith(
+                  amount: _value,
+                ),
+              );
+        },
+        decoration: InputDecoration(
+          labelText: 'Amount',
+        ),
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+        keyboardType: TextInputType.number,
+      ),
+    );
+  }
+
+  _transactionMode(
+      {required BuildContext context, required SalesReturn state}) {
+    return //------------Transaction Mode----
+        GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) =>
+              SalesReturnTransactionModeSelectModalBottomSheet(),
+        );
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.55,
+        height: MediaQuery.of(context).size.height * 0.05,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.grey.shade300,
+          borderRadius: BorderRadius.circular(3),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              EnumToString.convertToString(state.mode, camelCase: true),
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Icon(
+              CupertinoIcons.arrowtriangle_down,
+              color: Colors.black,
+              size: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _partySelect({required BuildContext context, required WidgetRef ref}) {
+    final state = ref.watch(saleReturnControllerProvider);
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) => PartySelectModal(
+            onSelectParty: (party) {
+              ref.watch(saleReturnControllerProvider.notifier).setState(
+                    state.data!.value.copyWith(partyLedger: party),
+                  );
+            },
+          ),
+        );
+      },
+      child: Container(
+        width: state.data!.value.mode == TransactionMode.credit
+            ? MediaQuery.of(context).size.width * 0.97
+            : MediaQuery.of(context).size.width * 0.55,
+        height: MediaQuery.of(context).size.height * 0.05,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Colors.grey.shade300,
+            borderRadius: BorderRadius.circular(3),
+            border: Border.all(
+              color: state.data!.value.partyLedger != null
+                  ? Colors.grey.shade300
+                  : Colors.red.shade300,
+            )),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              state.data!.value.partyLedger == null
+                  ? 'Please Select Party'
+                  : state.data!.value.partyLedger!.name,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Icon(
+              CupertinoIcons.arrowtriangle_down,
+              color: Colors.black,
+              size: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _partialPayment({required BuildContext context, required WidgetRef ref}) {
+    final state = ref.watch(saleReturnControllerProvider);
+    return PartialPaymentWidget(
+      onChange: (amount) {
+        ref.watch(saleReturnControllerProvider.notifier).setState(
+              state.data!.value.copyWith(partialPaymentAmount: amount),
+            );
+      },
+      defaultValue: state.data!.value.partialPaymentAmount,
+      maxAmount: state.data!.value.amount,
+    );
+  }
+
+  _particular({required BuildContext context, required WidgetRef ref}) {
+    final state = ref.watch(saleReturnControllerProvider);
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.95,
+      height: MediaQuery.of(context).size.height * 0.1,
+      child: TextFormField(
+        initialValue: state.data!.value.particular,
+        onChanged: (value) {
+          ref.watch(saleReturnControllerProvider.notifier).setState(
+                state.data!.value.copyWith(particular: value),
+              );
+        },
+        decoration: InputDecoration(
+          labelText: 'particular',
+        ),
+      ),
+    );
+  }
+}
