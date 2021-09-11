416836f18af9394e6cea4b243e57e4471f1f2d0c
William-Lalfakawma <willelune@gmail.com>
2021-09-11 16:57:45 -0500

    fix ledger

--- lib/books/tradingAccount/tradingAccountReport_screen.dart
+++ lib/books/tradingAccount/tradingAccountReport_screen.dart
@@ -1,87 +1,111 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:sentezel/books/tradingAccount/tradingAccount_controller.dart';
-import 'package:sentezel/books/widgets/reportTopBar_widget.dart';
-import 'tradingAccount_model.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_hooks/flutter_hooks.dart';
+// import 'package:hooks_riverpod/hooks_riverpod.dart';
+// import 'package:sentezel/books/tradingAccount/tradingAccount_controller.dart';
+// import 'package:sentezel/books/widgets/reportTopBar_widget.dart';
+// import 'tradingAccount_model.dart';
 
-class TradingAccountReportScreen extends HookConsumerWidget {
-  const TradingAccountReportScreen({Key? key}) : super(key: key);
+// class TradingAccountReportScreen extends HookConsumerWidget {
+//   const TradingAccountReportScreen({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final state = ref.watch(tradingAccountControllerProvider);
-    useEffect(() {
-      ref.watch(tradingAccountControllerProvider.notifier).loadData();
-    }, []);
-    return Material(
-      child: SafeArea(
-        child: Container(
-          child: Column(
-            children: [
-              ReportTopBarWidget(
-                  title: 'Trading Account',
-                  onGenerateExcel: () {},
-                  onGeneratePdf: () {},
-                  onClose: () {}),
-              state.when(data: (data) {
-                print('when data');
-                return _list(context: context, data: data, ref: ref);
-              }, loading: () {
-                print('inside loading');
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }, error: (error, stack) {
-                return Center(
-                  child: Text('error'),
-                );
-              })
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final state = ref.watch(tradingAccountControllerProvider);
+//     useEffect(() {
+//       ref.watch(tradingAccountControllerProvider.notifier).loadData();
+//     }, []);
+//     return Material(
+//       child: SafeArea(
+//         child: DefaultTabController(
+//           length: 2,
+//           child: TabBarView(
+//             children: [
+//               // expense side
+//               Column(
+//                 children: [
+//                   ReportTopBarWidget(
+//                       title: 'Trading Account',
+//                       onGenerateExcel: () {},
+//                       onGeneratePdf: () {},
+//                       onClose: () {}),
+//                   state.when(data: (data) {
+//                     print('when data');
+//                     return _expenselist(context: context, data: data, ref: ref);
+//                   }, loading: () {
+//                     print('inside loading');
+//                     return Center(
+//                       child: CircularProgressIndicator(),
+//                     );
+//                   }, error: (error, stack) {
+//                     return Center(
+//                       child: Text('error'),
+//                     );
+//                   }
+//                 )
+//               ],
+//             ),
+//             // Income side
+//             Column(
+//                 children: [
+//                   ReportTopBarWidget(
+//                       title: 'Trading Account',
+//                       onGenerateExcel: () {},
+//                       onGeneratePdf: () {},
+//                       onClose: () {}),
+//                   state.when(data: (data) {
+//                     print('when data');
+//                     return _list(context: context, data: data, ref: ref);
+//                   }, loading: () {
+//                     print('inside loading');
+//                     return Center(
+//                       child: CircularProgressIndicator(),
+//                     );
+//                   }, error: (error, stack) {
+//                     return Center(
+//                       child: Text('error'),
+//                     );
+//                   }
+//                 )
+//               ],
+//             ),
+//             ], 
+//           ),
+//         ),
+//       ),
+//     );
+//   }
 
-  _list(
-      {required BuildContext context,
-      required List<TradingAccount> data,
-      required WidgetRef ref}) {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: data.length,
-        itemBuilder: (context, index) {
-          return _listItem(
-            context: context,
-            item: data[index],
-            ref: ref,
-          );
-        },
-      ),
-    );
-  }
+//   _expenselist(
+//       {required BuildContext context,
+//       required List<TradingAccount> data,
+//       required WidgetRef ref}) {
+//     return Expanded(
+//       child: ListView.builder(
+//         itemCount: data.length,
+//         itemBuilder: (context, index) {
+//           return _expenselistItem(
+//             context: context,
+//             item: data[index],
+//             ref: ref,
+//           );
+//         },
+//       ),
+//     );
+//   }
 
-  _listItem(
-      {required BuildContext context,
-      required TradingAccount item,
-      required WidgetRef ref}) {
-    int balance = 0;
-    if (item.creditAmount > item.debitAmount) {
-      balance = item.creditAmount - item.debitAmount;
-    } else if (item.debitAmount > item.creditAmount) {
-      balance = item.debitAmount - item.creditAmount;
-    } else if (item.creditAmount == item.debitAmount) {
-      balance = 0;
-    }
-    return Row(
-      children: [
-        Text(item.ledgerName),
-        SizedBox(
-          width: 20,
-        ),
-        Text(balance.toString()),
-      ],
-    );
-  }
-}
+//   _expenselistItem(
+//       {required BuildContext context,
+//       required TradingAccount item,
+//       required WidgetRef ref}) {
+//     int balance = 0;
+//     return Row(
+//       children: [
+//         Text(item.expense.ledgerName),
+//         SizedBox(
+//           width: 20,
+//         ),
+//         Text(balance.toString()),
+//       ],
+//     );
+//   }
+// }
--- lib/books/tradingAccount/tradingAccount_controller.dart
+++ lib/books/tradingAccount/tradingAccount_controller.dart
@@ -1,8 +1,5 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:sentezel/books/ledger/ledgerTransaction/ledgerTransaction_model.dart';
-import 'package:sentezel/books/tradingAccount/expense_model.dart';
 import 'package:sentezel/books/tradingAccount/tradingAccount_model.dart';
-import 'package:sentezel/common/enums/debitOrCredit_enum.dart';
 import 'package:sentezel/common/enums/transactionType_enum.dart';
 import 'package:sentezel/newTransaction/data/transaction_repository.dart';
 import 'package:sentezel/settings/ledgerMaster/data/ledgerMasterType_enum.dart';
@@ -21,76 +18,66 @@ class TradingAccountController
   TradingAccountController(this._read) : super(AsyncValue.loading());
   loadData() async {
     try {
-      final ledgerMasterList = await _read(ledgerMasterRepositoryProvider)
+      List<TradingAccount> _tradingAccountList = [];
+      final _ledgerMasterDataList = await _read(ledgerMasterRepositoryProvider)
           .getList(type: LedgerMasterType.direct);
-
-      print('list chu ${ledgerMasterList.length}');
-
-      List<Expense> _directExpense = [];
-
-      List<TradingAccount> tradingAccountList = [];
-
-      //---------Iterate Ledger Master List----------
-      for (int i = 0; i < ledgerMasterList.length; i++) {
+      for (int i = 0; i < _ledgerMasterDataList.length; i++) {
+        List<Income> _incomeList = [];
+        List<Expense> _expenseList = [];
+        TradingAccount _tradingAccount =
+            TradingAccount(expense: _expenseList, income: _incomeList);
         final _transactionList = await _read(transactionRepositoryProvider)
             .getTransactionByLedgerMaster(
-                ledgerMasterId: ledgerMasterList[i].id);
-
-        List<LedgerTransaction> _ledgerTransactionList = [];
-        int _creditAmount = 0;
-        int _debitAmount = 0;
-        List<Expense> _expense = [];
-        List<Income> _income = [];
-        TradingAccount _tradingAccount = TradingAccount(
-          ledgerName: ledgerMasterList[i].name,
-          creditAmount: _creditAmount,
-          debitAmount: _debitAmount,
-          expense: _expense,
-          income: _income,
-        );
-
-        // print('no of list is ${_transactionList.length}');
-
+                ledgerMasterId: _ledgerMasterDataList[i].id);
         for (int j = 0; j < _transactionList.length; j++) {
-          LedgerTransaction _ledgerTransaction = LedgerTransaction(
-            amount: 0,
-            debitOrCredit: DebitOrCredit.credit,
-            transaction: _transactionList[j],
-          );
-
-          //---get transaction category
-          TransactionCategory _transactionCategory =
+          final TransactionCategory _transactionCategory =
               await _read(transactionCategoryRepositoryProvider)
                   .getItem(id: _transactionList[j].transactionCategoryId);
-
-          //---direct chu a ni vek sa tawh e, enge kan mamawh tak kha
-          //-- expense leh income a hran kha
-
-          if (_transactionCategory.transactionType == TransactionType.hralh ||
-              _transactionCategory.transactionType == TransactionType.lakluh) {
-            //heihi income , tihian a dik hleitheilo a, ka hrethiam chiahlo
-            //database hit lo hian test i lo siam dawn a mi le
-            //test in a sam berin ka ring
-            _tradingAccount.debitAmount += _transactionList[j].debitAmount;
-            _ledgerTransaction.amount = _transactionList[j].debitAmount;
-            _ledgerTransaction.debitOrCredit = DebitOrCredit.debit;
+          // * check if the transaction is income
+          if (_transactionCategory.transactionType == TransactionType.lakluh ||
+              _transactionCategory.transactionType == TransactionType.hralh) {
+            int _totalIncomeCredit = 0;
+            int _totalIncomeDebit = 0;
+            Income _income = Income(
+                ledgerName: _ledgerMasterDataList[i].name,
+                totalCredit: _totalIncomeCredit,
+                totalDebit: _totalIncomeDebit);
+            // check if the transacion is the particular ledger
+            if (_transactionList[j].debitSideLedger ==
+                _ledgerMasterDataList[i].id) {
+              _income.totalDebit += _transactionList[j].debitAmount;
+            }
+            if (_transactionList[j].creditSideLedger ==
+                _ledgerMasterDataList[i].id) {
+              _income.totalCredit += _transactionList[j].creditAmount;
+            }
+            _incomeList.add(_income);
+            // * check if the transaction is expense
           } else if (_transactionCategory.transactionType ==
                   TransactionType.lei ||
               _transactionCategory.transactionType ==
                   TransactionType.pekchhuah) {
-            _tradingAccount.creditAmount += _transactionList[j].creditAmount;
-            _ledgerTransaction.amount = _transactionList[j].creditAmount;
-            _ledgerTransaction.debitOrCredit = DebitOrCredit.credit;
+            int _totalExpenseCredit = 0;
+            int _totalExpenseDebit = 0;
+            Expense _expense = Expense(
+              totalCredit: _totalExpenseCredit,
+              totalDebit: _totalExpenseDebit,
+              ledgerName: _ledgerMasterDataList[i].name,
+            );
+            // check if the transacion is the particular ledger
+            if (_transactionList[j].debitSideLedger ==
+                _ledgerMasterDataList[i].id) {
+              _expense.totalDebit += _transactionList[j].debitAmount;
+            }
+            if (_transactionList[j].creditSideLedger ==
+                _ledgerMasterDataList[i].id) {
+              _expense.totalCredit += _transactionList[j].creditAmount;
+            }
+            _expenseList.add(_expense);
           }
-          _ledgerTransactionList.add(_ledgerTransaction);
-          //--- git hub ah lo push teh a, ka lo en ve ange.
-          // test chu lo ziak la aw
-
         }
-        //----add into the model of the calculated list
-        tradingAccountList.add(_tradingAccount);
+        _tradingAccountList.add(_tradingAccount);
       }
-      state = AsyncData(tradingAccountList);
     } catch (e) {
       print("error on traiding account controller ${e.toString()}");
     }
