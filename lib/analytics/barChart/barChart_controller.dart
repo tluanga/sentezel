3fd09231492d223db892cdab8bcc85661baa313a
tluanga <tluanga@live.com>
2021-09-15 13:27:39 -0700

    a

--- lib/analytics/barChart/barChart_controller.dart
+++ lib/analytics/barChart/barChart_controller.dart
@@ -1,5 +1,5 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:sentezel/analytics/barChart/barchart_model.dart';
+import 'package:sentezel/analytics/barchart/barchart_model.dart';
 import 'package:sentezel/new_transaction/data/transaction_repository.dart';
 
 final barChartControllerProvider =
@@ -7,8 +7,8 @@ final barChartControllerProvider =
         (ref) => BarChartController(ref.read)..loadData());
 
 class BarChartController extends StateNotifier<AsyncValue<BarChart>> {
-  Reader _read;
-  BarChartController(this._read) : super(AsyncLoading());
+  final Reader _read;
+  BarChartController(this._read) : super(const AsyncLoading());
 
   loadData() async {
     //----Read all transaction---
@@ -18,5 +18,8 @@ class BarChartController extends StateNotifier<AsyncValue<BarChart>> {
 
     //---Sort Transaction Data by Date----------
     transactionData.sort((a, b) => a.date.compareTo(b.date));
+    state = AsyncData(
+      BarChart(leftTitles: [], bottomTitles: [], barChartGroupDataList: []),
+    );
   }
 }
