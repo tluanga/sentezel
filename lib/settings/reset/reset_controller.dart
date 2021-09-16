7be6ce27c42c097cbd95da9f62479ac62531e876
tluanga <tluanga@live.com>
2021-09-16 16:03:47 -0700

    a

--- lib/settings/reset/reset_controller.dart
+++ lib/settings/reset/reset_controller.dart
@@ -1,6 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sentezel/settings/ledger_master/ledger_master_repository.dart';
 
 class ResetController extends StateNotifier<int> {
   final Reader _read;
   ResetController(this._read) : super(0);
+
+  reset() {
+    //--------Reset Ledger Master----------
+  }
 }
