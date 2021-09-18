4ea04b08dfd24df009a6130b0bd3af11bc21c622
tluanga <tluanga@live.com>
2021-09-18 22:55:24 +0530

    transaction category

--- lib/settings/transactionCategory/transaction_category_screen.dart
+++ lib/settings/transactionCategory/transaction_category_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:sentezel/common/ui/widget/floating_action_button_widget.dart';
 import 'package:sentezel/common/ui/widget/top_bar_widget.dart';
+import 'package:sentezel/settings/ledger_master/new_ledger_master_screen.dart';
 
 class TransactionCategoryScreen extends StatelessWidget {
   const TransactionCategoryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: SafeArea(
+    return Scaffold(
+      body: SafeArea(
         child: Column(
           children: [
             TopBarWidget(
@@ -18,6 +21,16 @@ class TransactionCategoryScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+      floatingActionButton: SentezelFloatingActionButton(
+        onPressed: () {
+          showCupertinoModalBottomSheet(
+            expand: true,
+            context: context,
+            backgroundColor: Colors.transparent,
+            builder: (context) => const NewLedgerMasterScreen(),
+          );
+        },
       ),
     );
   }
