416380dd534b7f6faa65d98aa2f67fb434ccc4bf
RcaPunte <69230622+RcaPunte@users.noreply.github.com>
2021-09-20 17:22:06 +0530

    Update pin change UI

--- lib/authentication/pin/pin_change_screen.dart
+++ lib/authentication/pin/pin_change_screen.dart
@@ -10,54 +10,138 @@ class PinChangeScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(pinControllerProvider);
-    return Material(
-      child: SafeArea(
-        child: Column(
-          children: [
-            TopBarWidget(
-                title: 'PinChange',
-                onClose: () {
-                  Navigator.pop(context);
-                }),
-            const Text('Change Your Pin'),
-            TextFormField(
-              decoration: const InputDecoration(labelText: ' Enter Pin'),
-              onChanged: (value) {
-                state.pin = int.parse(value);
-              },
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Re Enter Pin'),
-              onChanged: (value) {
-                // ref.read(pinControllerProvider.notifier).checkPassPhrase(value);
-              },
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Pass Phrase'),
-              onChanged: (value) {
-                state.passPhrase = value;
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.5,
-              height: MediaQuery.of(context).size.height * 0.05,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                borderRadius: BorderRadius.circular(10),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Material(
+        child: SafeArea(
+          child: Column(
+            children: [
+              TopBarWidget(
+                  title: 'Pin Change',
+                  onClose: () {
+                    Navigator.pop(context);
+                  }),
+              Spacer(flex: 1),
+              const Text('Change Your Pin'),
+              SizedBox(
+                height: 10,
               ),
-              child: const Center(child: Text('Submit')),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Icon(
-              CupertinoIcons.smallcircle_circle,
-              size: 300,
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      decoration: BoxDecoration(
+                          color: Colors.white, // Colors.grey.shade200,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 2,
+                              blurRadius: 7,
+                              offset: const Offset(
+                                  0, 3), // changes position of shadow
+                            ),
+                          ]),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: ' Enter Pin', border: InputBorder.none),
+                        onChanged: (value) {
+                          state.pin = int.parse(value);
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      decoration: BoxDecoration(
+                          color: Colors.white, // Colors.grey.shade200,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 2,
+                              blurRadius: 7,
+                              offset: const Offset(
+                                  0, 3), // changes position of shadow
+                            ),
+                          ]),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'Re Enter Pin',
+                            border: InputBorder.none),
+                        onChanged: (value) {
+                          // ref.read(pinControllerProvider.notifier).checkPassPhrase(value);
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      decoration: BoxDecoration(
+                          color: Colors.white, // Colors.grey.shade200,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 2,
+                              blurRadius: 7,
+                              offset: const Offset(
+                                  0, 3), // changes position of shadow
+                            ),
+                          ]),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'Pass Phrase', border: InputBorder.none),
+                        onChanged: (value) {
+                          state.passPhrase = value;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Spacer(
+                flex: 3,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.5,
+                height: MediaQuery.of(context).size.height * 0.06,
+                decoration: BoxDecoration(
+                    color: Color(0xff0d47a1),
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 2,
+                        blurRadius: 7,
+                        offset:
+                            const Offset(0, 3), // changes position of shadow
+                      ),
+                    ]),
+                child: const Center(
+                    child: Text(
+                  'Submit',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
         ),
       ),
     );
